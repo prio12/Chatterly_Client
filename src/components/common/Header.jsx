895c208f479e3e22e45a80d56813010337df2330
prio12 <maksudurprio.mp@gmail.com>
2025-01-19 00:40:20 +0600

    header added for small screen

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,6 +6,9 @@ import { FaRegMessage } from 'react-icons/fa6';
 import { SlPeople } from 'react-icons/sl';
 import { Link } from 'react-router';
 import cIcon from '../../assets/icon/letter-c (1).png';
+import { RiMenu2Fill } from 'react-icons/ri';
+import { LuMenu } from 'react-icons/lu';
+import { IoIosSearch } from 'react-icons/io';
 
 const Header = () => {
   return (
@@ -55,6 +58,26 @@ const Header = () => {
             <div className="mask mask-squircle w-10">
               <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
             </div>
+          </div>
+        </div>
+      </div>
+      {/* header for small screen */}
+      <div className=" md:hidden flex items-center sticky top-0 justify-between lg:hidden py-4 px-2 w-full bg-slate-400">
+        <div>
+          <RiMenu2Fill className="text-xl" />
+        </div>
+        <div className="w-12">
+          <img src={cIcon} className="w-full" alt="" />
+        </div>
+        <div>
+          <LuMenu className="text-xl" />
+        </div>
+        <div>
+          <IoIosSearch className="text-xl" />
+        </div>
+        <div className="avatar">
+          <div className="mask mask-squircle w-10">
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
           </div>
         </div>
       </div>
