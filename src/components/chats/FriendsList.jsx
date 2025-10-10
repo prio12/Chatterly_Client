fbe9bbccec3b2f0da5e18bb3240220910f423b1b
prio12 <maksudurprio.mp@gmail.com>
2025-10-11 01:08:31 +0600

    fixed active inActive friendsList

--- src/components/chats/FriendsList.jsx
+++ src/components/chats/FriendsList.jsx
@@ -1,83 +1,61 @@
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router';
+import { useNavigate } from 'react-router';
 import DefaultProfilePIcture from '../profile/DefaultProfilePIcture';
+import { useEffect, useState } from 'react';
 
 const FriendsList = ({
-  myConnections,
-  activeConnections,
+  myConnections = [],
+  activeConnections = [],
   handleInitiateChat,
   isSmall,
 }) => {
+  const [margedConnections, setMargedConnections] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const activeIds = new Set(
+      activeConnections.map((con) => con?.myConnection?._id)
+    );
+
+    const margedConn = [
+      ...activeConnections.map((conn) => ({ ...conn, status: 'active' })),
+      ...myConnections.filter(
+        (conn) => !activeIds.has(conn?.myConnection?._id)
+      ),
+    ];
+
+    if (margedConn?.length) {
+      setMargedConnections(margedConn);
+    }
+  }, [activeConnections, myConnections]);
+
+  const handleOpenChat = (user) => {
+    if (isSmall) {
+      navigate(`/chats/${user?.uid}`);
+    } else {
+      handleInitiateChat(user);
+    }
+  };
+
   return (
     <div className="w-full flex items-center gap-5 overflow-x-auto my-5 p-5 no-scrollbar bg-slate-100 ">
-      {activeConnections?.length > 0
-        ? activeConnections?.map((connection) => (
-            <div key={connection?._id} className="bg-white p-3">
-              {isSmall ? (
-                <Link to={`/chats/${connection?.myConnection?.uid}`}>
-                  <div className="avatar online cursor-pointer">
-                    <div className="w-12 rounded-full">
-                      {connection?.myConnection?.profilePicture ? (
-                        <img src={connection?.myConnection?.profilePicture} />
-                      ) : (
-                        <DefaultProfilePIcture />
-                      )}
-                    </div>
-                  </div>
-                </Link>
-              ) : (
-                <div
-                  onClick={() => handleInitiateChat(connection?.myConnection)}
-                  className="avatar online cursor-pointer"
-                >
-                  <div className="w-12 rounded-full">
-                    {connection?.myConnection?.profilePicture ? (
-                      <img src={connection?.myConnection?.profilePicture} />
-                    ) : (
-                      <DefaultProfilePIcture />
-                    )}
-                  </div>
-                </div>
-              )}
-              <p className="mt-2 font-semibold text-sm">
-                {connection?.myConnection?.name}
-              </p>
-            </div>
-          ))
-        : myConnections?.map((connection) => (
-            <div key={connection?._id} className="bg-white p-3">
-              {isSmall ? (
-                <Link to={`/chats/${connection?.myConnection?.uid}`}>
-                  {' '}
-                  <div className="avatar offline cursor-pointer">
-                    <div className="w-12 rounded-full">
-                      {connection?.myConnection?.profilePicture ? (
-                        <img src={connection?.myConnection?.profilePicture} />
-                      ) : (
-                        <DefaultProfilePIcture />
-                      )}
-                    </div>
-                  </div>
-                </Link>
-              ) : (
-                <div
-                  onClick={() => handleInitiateChat(connection?.myConnection)}
-                  className="avatar offline cursor-pointer"
-                >
-                  <div className="w-12 rounded-full">
-                    {connection?.myConnection?.profilePicture ? (
-                      <img src={connection?.myConnection?.profilePicture} />
-                    ) : (
-                      <DefaultProfilePIcture />
-                    )}
-                  </div>
-                </div>
-              )}
-              <p className="mt-2 font-semibold text-sm">
-                {connection?.myConnection?.name}
-              </p>
-            </div>
-          ))}
+      {margedConnections?.map((conn) => (
+        <div
+          key={conn?.myConnection?._id}
+          className={`avatar  cursor-pointer ${
+            conn?.status === 'active' ? 'online' : 'offline'
+          }`}
+          onClick={() => handleOpenChat(conn?.myConnection)}
+        >
+          <div className="w-12 rounded-full">
+            {conn?.myConnection?.profilePicture ? (
+              <img src={conn?.myConnection?.profilePicture} />
+            ) : (
+              <DefaultProfilePIcture />
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
